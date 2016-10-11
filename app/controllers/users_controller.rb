bfd8229c0b5abc0943d1e6217e92e036829ded64
Mayowa Adegbola <mayowa.adegbola@andela.com>
2016-10-11 16:54:22 +0100

    Implement session feature for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       flash[:success] = "Welcome to Chomp!"
+      log_in(@user)
       redirect_to root_path
     else
       render 'new'
